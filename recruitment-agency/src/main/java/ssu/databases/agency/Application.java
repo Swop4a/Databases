7c726c5889f3c8f1f7a4bf104345a22192128904
Swop4a <alexandr300896@gmail.com>
2017-05-12 17:27:06 +0400

    add interface

---
+++ recruitment-agency/src/main/java/ssu/databases/agency/Application.java
@@ -0,0 +1,14 @@
+package ssu.databases.agency;
+
+/**
+ * Spring Boot application launcher
+ *
+ * @author Swop4a
+ * @version 1.0
+ */
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
